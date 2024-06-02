c12fef08ff824490a232dfe3315a43a650fe86ea
Emre <emre.sener@sigmalis.ch>
2024-06-02 18:52:54 +0200

    Fixing responsive issues

--- assets/js/main.js
+++ assets/js/main.js
@@ -68,7 +68,6 @@
 	// Header.
 		if ($accueil.length > 0
 		&&	$header.hasClass('alt')) {
-			console.log($accueil.length);
 			$window.on('resize', function() { $window.trigger('scroll'); });
 
 			$accueil.scrollex({
